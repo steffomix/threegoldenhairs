9413c3f79bba1f8e91ef4c4d27b0b92086ac3ca2
steffomix <steffomix@gmail.com>
2018-08-13 12:52:34 +0200

    render pages

--- src/game/finalize/render-days.php
+++ src/game/finalize/render-days.php
@@ -1,45 +1,124 @@
-<?php
-
+<?php 
 error_reporting(E_ALL | E_NOTICE);
+set_time_limit(0);
+ignore_user_abort(true);
+
+require(dirname(__FILE__) . '/html2pdf/vendor/autoload.php');
+require(dirname(__FILE__) . '/html2pdf/src/Html2Pdf.php');
+
+function convert($source, $dest){
+	$html2pdf = new \Spipu\Html2Pdf\Html2Pdf('P', 'A4', 'en');
+	$html2pdf->writeHTML($source);
+	$f = fopen($dest, 'w');
+	fwrite($f, $html2pdf->output(null, 'S'));
+}
 
 function zf($n){
 	return strlen($n) > 1 ? $n : '0'.$n;
 }
 
+define('PAGE', '+++');
+define('HOUR', '###');
+define('MINUTE', '#');
 
-$home = '../../GIT/threegoldenhairs/src/game/server/';
-$days = [$home.'level 1 - explore the lands/story level 1.txt',
-	$home.'level 2 - climb the mountain/story level 2.txt',
-	$home.'level 3 - revive the dead/story level 3.txt',
-	$home.'level 4 - day of days/the end.txt'];
+$nl = "<br />\n";
+$pages = array();
+$page = '';
+
+$home = '../../GIT/threegoldenhairs2/src/game/finalize/';
+$days = [$home.'day-1.txt',
+	$home.'day-2.txt',
+	$home.'day-3.txt'];
 	
-$names = ['Explore the Lands', 'Climb the Mountain', 'Revive the Dead', 'Day of Days'];
-$d = 1;
-$txt = '';
-foreach($days as $day){
-	$hours = explode('###', trim(file_get_contents($day)));
-	array_shift($hours);
-	$h = 1;
-	$txt .= sprintf("<h1>Day %s - %s</h1>", $d, $names[$d-1]);
-	foreach($hours as $hour){
-		$txt .= "<p>";
-		$minutes = explode('#', trim($hour));
-		
-		$head = array_shift($minutes);
-		$txt .= sprintf("<b>Day %s, %s:00 %s</b><br/>", $d, zf($h), $head);
-		$m = 0;
-		foreach($minutes as $minute){
-			$minute = trim($minute);
-			$m ++;
-			$txt .= sprintf("%s-%s:%s %s<br/>", $d, zf($h), zf($m), $minute);
-		}
-		$h ++;
-		$txt .= '</p>';
-	}
-	$d++;
-}
-echo $txt;
-// echo is_dir($level1) ? 1 : 0;
+$img = $home.'img/';
 
-exit();
-phpinfo();
+define('PO', '<page backtop="7mm" backbottom="7mm" backleft="10mm" backright="10mm"><page_header>%s</page_header><page_footer>%s</page_footer>%s</page> ');
+
+function page($ckg){
+	global $pages;
+	if(strpos($ckg, PAGE) !== false){
+		$ckg = str_replace(PAGE, '', $ckg);
+		$pages[] = '';
+	}
+	$pages[count($pages)-1] .=  $ckg;
+	
+}
+
+function day($day, $d){
+	$hours = explode(HOUR, trim($day));
+	array_shift($hours);
+	hours($hours, $d);
+}
+
+
+function hours($hours, $d){
+	global $nl;
+	$h = 1;
+	foreach($hours as $hour){
+		$minutes = explode(MINUTE, $hour);
+		$head = array_shift($minutes); 
+		
+		page( sprintf("<b>Day %s, %s:00 %s</b>$nl", $d, zf($h), trim($head)));
+		minutes($minutes, $h);
+		//$p .= $nl . $nl;
+		//page($p);
+		$h ++;
+	}
+	
+	// $pages[count($pages)-1] .= $txt;
+}
+
+function minutes($minutes, $h){
+	global $page;
+	$m = 1;
+	foreach($minutes as $minute){
+		$minute = preg_replace(
+			'/.*\{(.*)\[\[(.*)\]\]\}/mi',
+			'<div><img style="width: 50%; height: auto;" src="img/$2" /><br/><i style="color: #a0a0a0; font-size: .8em;">$1</i></div>',
+			$minute);
+		$minute = preg_replace(
+			'/.*\{(.*)\}/mi',
+			'<i style="color: #a0a0a0; font-size: .8em;">$1</i>',
+			$minute);
+		$m ++;
+		page( sprintf("%s:%s %s<br/>", zf($h), zf($m), trim($minute)));
+	}
+}
+	
+
+
+function processFile($day, $d){
+	global $pages;
+	$d ++;
+	$names = ['Explore the Lands', 'Climb the Mountain', 'Revive the Dead', 'Day of Days'];
+	$pages[] = "<h1>Day $d: " . $names[$d-1] . "</h1>\n";
+	day(file_get_contents($day) , $d);
+	//if($page != '') $pages[] = $page;
+
+	foreach($pages as $i => $page){
+		$pages[$i] = sprintf('<page backtop="10mm" backbottom="10mm" backleft="20mm" backright="10mm"><page_header>%s</page_header><page_footer>%s</page_footer>%s</page>',
+	'', '', $page);
+	}
+	$txt = implode("\n\n", $pages);
+	//echo $txt; exit();
+	convert($txt, $names[$d - 1] . ".pdf");
+}
+
+$txt = '';
+if(isset($_GET['d'])){
+	$d = intval($_GET['d']);
+	$d = max(min($d, 3), $d);
+	$txt = processFile($days[$d-1], $d-1);
+}else{
+	foreach($days as $d => $day){
+		$txt .= processFile($day, $d);
+		break;
+	}
+}
+
+
+
+
+
+
+
