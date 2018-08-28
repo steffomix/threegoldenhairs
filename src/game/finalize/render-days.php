b413a78b2803f5eaf3f1573aaaa241ce15e175c2
steffomix <steffomix@gmail.com>
2018-08-28 18:06:36 +0200

    2. bedtime story, rape soul

--- src/game/finalize/render-days.php
+++ src/game/finalize/render-days.php
@@ -1,4 +1,12 @@
+<html>
+<head>
+</head>
+<body>
+set getvar "pdf" to create pdf<br>
+set getvar "day=1-3" for single day<br>
+
 <?php 
+
 error_reporting(E_ALL | E_NOTICE);
 set_time_limit(0);
 ignore_user_abort(true);
@@ -21,7 +29,6 @@ define('PAGE', '+++');
 define('HOUR', '###');
 define('MINUTE', '#');
 
-$nl = "<br />\n";
 $pages = array();
 $page = '';
 
@@ -46,26 +53,24 @@ function page($ckg){
 
 function day($day, $d){
 	$hours = explode(HOUR, trim($day));
+	echo 'Count hours: ' . count($hours).'<p>';
 	array_shift($hours);
 	hours($hours, $d);
+	echo '</p>';
 }
 
 
 function hours($hours, $d){
-	global $nl;
 	$h = 1;
 	foreach($hours as $hour){
 		$minutes = explode(MINUTE, $hour);
 		$head = array_shift($minutes); 
-		
-		page( sprintf("<b>Day %s, %s:00 %s</b>$nl", $d, zf($h), trim($head)));
+		$head = sprintf("<b>Day %s, %s:00 %s</b><br />\n", $d, zf($h), trim($head));
+		echo $head;
+		page($head);
 		minutes($minutes, $h);
-		//$p .= $nl . $nl;
-		//page($p);
 		$h ++;
 	}
-	
-	// $pages[count($pages)-1] .= $txt;
 }
 
 function minutes($minutes, $h){
@@ -73,12 +78,12 @@ function minutes($minutes, $h){
 	$m = 1;
 	foreach($minutes as $minute){
 		$minute = preg_replace(
-			'/.*\{(.*)\[\[(.*)\]\]\}/mi',
-			'<div><img style="width: 50%; height: auto;" src="img/$2" /><br/><i style="color: #a0a0a0; font-size: .8em;">$1</i></div>',
+			'/(.*)\{(.*)\[\[(.*)\]\]\}(.*)/mi',
+			'<div>$1<img style="width: 50%; height: auto;" src="img/$3" /><br/><i style="color: #a0a0a0; font-size: .8em;">{$2}</i>$4</div>',
 			$minute);
 		$minute = preg_replace(
-			'/.*\{(.*)\}/mi',
-			'<i style="color: #a0a0a0; font-size: .8em;">$1</i>',
+			'/(.*)\{(.*)\}(.*)/mi',
+			'$1<i style="color: #a0a0a0; font-size: .8em;">{$2}</i>$3',
 			$minute);
 		$m ++;
 		page( sprintf("%s:%s %s<br/>", zf($h), zf($m), trim($minute)));
@@ -93,20 +98,20 @@ function processFile($day, $d){
 	$names = ['Explore the Lands', 'Climb the Mountain', 'Revive the Dead', 'Day of Days'];
 	$pages[] = "<h1>Day $d: " . $names[$d-1] . "</h1>\n";
 	day(file_get_contents($day) , $d);
-	//if($page != '') $pages[] = $page;
-
 	foreach($pages as $i => $page){
-		$pages[$i] = sprintf('<page backtop="10mm" backbottom="10mm" backleft="20mm" backright="10mm"><page_header>%s</page_header><page_footer>%s</page_footer>%s</page>',
-	'', '', $page);
+		$pages[$i] = sprintf('<page backtop="10mm" backbottom="10mm" backleft="20mm" backright="10mm"><page_header>%s</page_header><page_footer>%s</page_footer>Page %s / %s</page>',
+	'', $i, count($pages), $page);
 	}
 	$txt = implode("\n\n", $pages);
-	//echo $txt; exit();
-	convert($txt, $names[$d - 1] . ".pdf");
+	echo $txt; 
+	//exit();
+	isset($_GET['pdf']) && convert($txt, $names[$d - 1] . ".pdf");
+	
 }
 
 $txt = '';
-if(isset($_GET['d'])){
-	$d = intval($_GET['d']);
+if(isset($_GET['day'])){
+	$d = intval($_GET['day']);
 	$d = max(min($d, 3), $d);
 	$txt = processFile($days[$d-1], $d-1);
 }else{
